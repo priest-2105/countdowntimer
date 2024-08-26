32ea40222d1ad94f82eab241648b17c4cb71ac7e
priest-2105 <fawzybailey782@gmail.com>
2024-08-26 19:21:51 +0100

    style init

---
+++ super.js
@@ -0,0 +1,34 @@
+const newYears = '1 Jan 2025';
+
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+
+
+function countDown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor((totalSeconds % (3600 * 24)) / 3600);
+    const minutes = Math.floor((totalSeconds / 60 ) % 24 ) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;   
+    secondsEl.innerHTML = seconds;
+
+}
+
+// initial call 
+countDown();
+
+
+setInterval(countDown, 1000)
