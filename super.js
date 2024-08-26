858dc21313768a1871ba292ac64cd6a7d8ac7a63
priest-2105 <fawzybailey782@gmail.com>
2024-08-26 20:50:33 +0100

    commit

--- super.js
+++ super.js
@@ -1,18 +1,38 @@
-const newYears = '1 Jan 2025';
+let defaultCountdownDate = '1 Jan 2025';
+
 
 
 const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
+const countdownTitle_i = document.getElementById('countdownTitle');
+const countdownDate_i = document.getElementById('countdownDate');
+const startButton_b = document.getElementById('startButton');
+
+
+
+
+startButton_b.addEventListener('click',() => {
+    // const countdownTitleText_p = document.getElementById('countdownTitleText');
+
+    defaultCountdownDate = countdownDate_i.value 
+    document.getElementById('countdownTitleText').innerHTML = countdownTitle_i.value 
+    startButton_b.disabled = true;
+    countDown();
+ });
+
+
+
+
 
 
 
 function countDown() {
-    const newYearsDate = new Date(newYears);
+    const defaultCountdownDateDate = new Date(defaultCountdownDate);
     const currentDate = new Date();
 
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const totalSeconds = (defaultCountdownDateDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor((totalSeconds % (3600 * 24)) / 3600);
@@ -27,8 +47,16 @@ function countDown() {
 
 }
 
+
+function formatTime(time){
+    return time < 10  ? `0${time}` : time;
+}
+
 // initial call 
 countDown();
 
 
 setInterval(countDown, 1000)
+
+
+
